c4a61761699569f645a1d328de625fa515ab3ad9
Ruani Filipe <dev.ruanifilipe@gmail.com>
2023-03-08 21:37:26 -0300

    completed task

--- src/controllers/promptController.js
+++ src/controllers/promptController.js
@@ -1,7 +1,24 @@
+const InputPrompt = require('../model/InputModel');
+const openai = require('../config/openai');
+
 module.exports = {
   async sendText(req, res) {
-    return res.status(200).json({
-      message: 'caio castro chegou no rolê',
-    });
+    const openaiAI = openai.configuration();
+    const inputModel = new InputPrompt(req.body);
+
+    try {
+      const response = await openaiAI.createCompletion(
+        openai.textCompletion(inputModel)
+      );
+      return response.status(200).json({
+        sucess: true,
+        data: response.data.choices[0].text,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        sucess: false,
+        error: error.response ? error.response.data : 'tem erro no request',
+      });
+    }
   },
 };
